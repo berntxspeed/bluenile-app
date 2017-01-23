6f6961b678938f1f6176cc570c7a900fa0967a2f
Vitali-VA <vitali.alexander@gmail.com>
2017-01-22 20:30:34 -0500

    initial data_builder commit on save, remove, show queries

---
+++ server/app/data_builder/services/data_builder_query.py
@@ -0,0 +1,37 @@
+# from server.app.stats.services.classes.db_data_loader import MongoDataLoader
+from server.app.stats.services.classes.db_data_loader import MongoDataLoader
+
+
+class DataBuilderQuery(object):
+    def __init__(self, client_instance):
+        self._primary_key = 'name'
+        self._db_name = 'simple_di_flask_dev'
+        self._collection_name = 'data_builder_qs'
+
+        self._db = client_instance[self._db_name]
+        self._collection = self._db[self._collection_name]
+
+    def get_all_queries(self):
+        all_queries = []
+        try:
+            for a_query in self._collection.find():
+                all_queries.append(a_query)
+
+            return True, all_queries
+        except Exception as e:
+            return False, 'Get All Queries Failed: {0}'.format(str(e))
+
+    def save_query(self, search_query):
+        item_loader = MongoDataLoader(self._collection, [self._primary_key])
+        try:
+            item_loader.load_to_db(search_query)
+            return True, True
+        except Exception as e:
+            return False, 'Saving Query Failed: {0}'.format(str(e))
+
+    def remove_query(self, query_name):
+        try:
+            self._collection.remove({self._primary_key: query_name})
+            return True, True
+        except Exception as e:
+            return False, 'Removing Query Failed: {0}'.format(str(e))
