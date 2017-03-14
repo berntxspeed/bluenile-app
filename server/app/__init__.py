deb1bf9724dbc2522aad224b189c640f1e396287
Val Petrov <vp@vayoola.com>
2017-03-13 22:17:01 -0400

    fix session issues

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -60,7 +60,9 @@ def create_injector(app=None):
 def init_db(app):
     from .common.models import db, SendJob, EmlSend, EmlOpen, EmlClick, Customer
     db.init_app(app)
-
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.orm import scoped_session
+    db.session = scoped_session(sessionmaker())
     # create the Flask-Restless API manager
     manager = Restless.APIManager(app, flask_sqlalchemy_db=db)
 
