8c41fd26d83b015f8db2fd0b2f961d961cabfb70
Bernt Gudmestad <bernt@xspeed.co>
2017-05-11 15:42:03 -0700

    forgot to move main/index to login_required

--- server/app/main/views.py
+++ server/app/main/views.py
@@ -1,8 +1,15 @@
 from flask import request, redirect
+from flask_login import login_required
 
 from ..common.views import context_processors
 from . import main
 from ..common.views.decorators import templated
+
+
+@main.before_request
+@login_required
+def before_request():
+    pass
 
 @main.before_request
 def before_request():
