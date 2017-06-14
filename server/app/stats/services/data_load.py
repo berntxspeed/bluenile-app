b2b2d1ce8bd448b6b58e1916b33fa57257dd7da6
Vitali <vv@Vitalis-MacBook-Pro.local>
2017-06-14 17:01:25 -0400

    catch up on data_load logic

--- server/app/stats/services/data_load.py
+++ server/app/stats/services/data_load.py
@@ -1,59 +1,252 @@
+import copy
+import os
 import requests
-import traceback, sys
-from pprint import pprint as pp 
+import sys
+import traceback
+import yaml
 
 from .classes.api_data import ApiData, ApiDataToSql, ApiDataToMongo
 from .classes.ftp_file import ZipFile, CsvFile
+from .mongo_user_config_loader import MongoUserApiConfigLoader
 from ...common.services import DbService
 from ...common.models import StgEmlSend, EmlSend, StgEmlOpen, EmlOpen, StgEmlClick, EmlClick, StgSendJob, SendJob, Customer, Purchase, WebTrackingEvent, WebTrackingPageView, WebTrackingEcomm
 
+
+# user specific: authentication + domain
+"""
+user_api_config = {
+    'magento':  {'domain': "http://127.0.0.1:32768",# domain address before 'index.php'
+                 'token': "npk3nc7gyhn8leab9baifl5075q45uhl"
+                 },
+
+    'x2crm':    {'domain': "http://demo.x2crm.com",
+                 'token': "YWRtaW46dGVzdA=="
+                 },
+
+    'shopify':  {'domain': "https://@xspeed.myshopify.com",
+                 'id': '20627b91731e8d8ee338bf786ae29feb',
+                 'secret': '045febd59c1d2acc3d00ac309360ea46'
+                 },
+
+    'bigcommerce':  {'domain': "https://store-vd63texh7u.mybigcommerce.com",
+                     'id': 'test',
+                     'secret': '07d63370ed9ddb2eee9143acb91b18af38cb5b9d'
+                     },
+
+    'stripe':   {'domain': "https://api.stripe.com/v1",
+                 'id': 'sk_test_C9SgTuKd9DN2PT6hFLPMzlts',
+                 'secret': ''
+                 }
+}
+"""
+
+# general config based on data_source
+"""
+api_config = {
+    'magento': {
+        'headers': {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '
+        },
+        'params': None,
+        'customer': {
+            'endpoint': "/index.php/rest/V1/customers/search?searchCriteria",
+            'primary_keys': ['customer_id'],
+            'json_data_keys': 'items',
+            'db_field_map': dict(customer_id='id',
+                                 email_address='email',
+                                 fname='firstname',
+                                 lname='lastname',
+                                 created_at='created_at',
+                                 # marketing_allowed='accepts_marketing',
+                                 # purchase_count='orders_count',
+                                 # total_spent_so_far='total_spent'
+                                 )
+        },
+        'purchase': {
+            'endpoint': "/index.php/rest/V1/orders?searchCriteria",
+            'primary_keys': ['purchase_id'],
+            'json_data_keys': 'items',
+            'db_field_map': dict(purchase_id='quote_id',
+                                 customer_id='customer_id',
+                                 created_at='created_at',
+                                 price='total_invoiced',
+                                 # is_paid='financial_status',
+                                 # referring_site='referring_site',
+                                 # landing_site='landing_site',
+                                 # browser_ip='browser_ip',
+                                 # user_agent='client_details.user_agent'
+                                 )
+        }
+    },
+
+    'x2crm': {
+        'headers': {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic '
+        },
+        'params': None,
+        'customer': {
+            'endpoint': "/index.php/api2/Contacts/",
+            'primary_keys': ['customer_id'],
+            'json_data_keys': None,
+
+            'db_field_map': dict(customer_id='id',
+                                 email_address='email',
+                                 fname='firstName',
+                                 city='city',
+                                 interest_area='interest',
+                                 lname='lastName',
+                                 created_at='createDate',
+                                 # marketing_allowed='accepts_marketing',
+                                 # purchase_count='orders_count',
+                                 # total_spent_so_far='total_spent'
+                                 )
+        }
+    },
+
+    'shopify': {
+        'headers': {'Content-Type': 'application/json'},
+        'params': None,
+        'purchase': {
+            # os.getenv('SHOPIFY_PURCHASE_API_ENDPOINT'),
+            'endpoint': "/admin/orders.json",
+            'primary_keys': ['purchase_id'],
+            'json_data_keys': 'orders',
+            'db_field_map': dict(purchase_id='id',
+                                 customer_id='customer.id',
+                                 created_at='created_at',
+                                 price='total_price',
+                                 is_paid='financial_status',
+                                 referring_site='referring_site',
+                                 landing_site='landing_site',
+                                 browser_ip='browser_ip',
+                                 user_agent='client_details.user_agent')
+        },
+        'customer': {
+            'endpoint': "/admin/customers.json",
+            'primary_keys': ['customer_id'],
+            'json_data_keys': 'customers',
+            'db_field_map': dict(customer_id='id',
+                                 email_address='email',
+                                 fname='first_name',
+                                 lname='last_name',
+                                 marketing_allowed='accepts_marketing',
+                                 created_at='created_at',
+                                 purchase_count='orders_count',
+                                 total_spent_so_far='total_spent')
+        }
+    },
+    'bigcommerce': {
+        'headers': {'Content-Type': 'application/json'},
+        'params': None,
+        'purchase': {
+            'endpoint': "/api/v2/orders.json",
+            'primary_keys': ['purchase_id'],
+            'json_data_keys': None,
+            'db_field_map': dict(purchase_id='id',
+                                 customer_id='customer_id',
+                                 created_at='date_created',
+                                 price='total_ex_tax',
+                                 is_paid='payment_status',
+                                 browser_ip='ip_address')
+        },
+        'customer': {
+            'endpoint': "/api/v2/customers.json",
+            'primary_keys': ['customer_id'],
+            'json_data_keys': None,
+            'db_field_map': dict(customer_id='id',
+                                 email_address='email',
+                                 fname='first_name',
+                                 lname='last_name',
+                                 marketing_allowed='accepts_marketing',
+                                 created_at='date_created')
+                                 # purchase_count='orders_count',
+                                 # total_spent_so_far='total_spent')
+        }
+    },
+    'stripe': {
+        'headers': {'Content-Type': 'application/json'},
+        'params': None,
+        'customer': {
+            'endpoint': "/customers",
+            'primary_keys': ['customer_id'],
+            'json_data_keys': 'data',
+            'db_field_map': dict(customer_id='id',
+                                 email_address='email',
+                                 # fname='first_name',
+                                 # lname='last_name',
+                                 # marketing_allowed='accepts_marketing',
+                                 created_at='created')
+            # purchase_count='orders_count',
+            # total_spent_so_far='total_spent')
+        }
+    }
+}
+"""
 
 class DataLoadService(DbService):
 
     def __init__(self, config, db, logger, mongo):
         super(DataLoadService, self).__init__(config, db, logger)
         self.mongo = mongo
+        self.data_type_map = {'customer': Customer,
+                              'purchase': Purchase
+                              }
+        with open('api_config.yml') as api_config_file:
+            self.data_load_config = yaml.load(api_config_file)
+
+        self.user_api_config = MongoUserApiConfigLoader(self.mongo.db).get_user_api_config()
 
     def exec_safe_session(self, load_func=None, *args):
         if load_func:
             try:
                 load_func(*args)
-            except:
+            except Exception as exc:
                 self.db.session.rollback()
+                raise type(exc)('DataLoad Error: {0}: {1}'.format(type(exc).__name__, exc.args))
             finally:
                 self.db.session.remove()
 
-    def load_customers(self):
-        # load customer table here
-        config = self.config
-        customer_data_source = config.get('CUSTOMER_DATA_SOURCE')
-        apicreds = config.get('EXT_DATA_CREDS')[customer_data_source]
-        auth = (apicreds['id'], apicreds['secret'])
-        endpoint = apicreds['endpoint']
-        headers = {'Content-Type': 'application/json'}
-        params = None #dict(ids='3211139395,3372597187')
-        db_field_map = dict(customer_id='id',
-                            email_address='email',
-                            fname='first_name',
-                            lname='last_name',
-                            marketing_allowed='accepts_marketing',
-                            created_at='created_at',
-                            purchase_count='orders_count',
-                            total_spent_so_far='total_spent')
-        primary_keys = ['customer_id']
-        json_data_keys = 'customers'
+    def get_api_args(self, data_source, data_type):
+        vendor_config = self.data_load_config.get(data_source, {})
+        api_args = copy.copy(vendor_config.get(data_type, {}))
 
-        ad1 = ApiDataToSql(endpoint=endpoint,
-                      auth=auth,
-                      headers=headers,
-                      params=params,
-                      db_session=self.db.session,
-                      db_model=Customer,
-                      primary_keys=primary_keys,
-                      db_field_map=db_field_map,
-                      json_data_keys=json_data_keys)
+        user_config = None
 
-        ad1.load_data()
+        # TODO: check for False status, log error
+        status, vendor_configs = self.user_api_config
+        if status is True:
+            for vendor_user_config in vendor_configs:
+                if vendor_user_config.get('data_source') == data_source:
+                    user_config = vendor_user_config
+                    break
+
+        api_args['headers'] = copy.copy(vendor_config.get('headers'))
+        if 'token' in user_config.keys():
+            api_args['headers']['Authorization'] += user_config['token']
+        elif 'id' in user_config.keys():
+            api_args['auth'] = (user_config['id'], user_config['secret'])
+
+        if 'endpoint' in api_args:
+            api_args['endpoint'] = user_config['domain'] + api_args['endpoint']
+        elif 'pagination' in api_args:
+            for a_key in api_args['pagination'].keys():
+                if a_key.endswith('endpoint') and not api_args['pagination'][a_key].startswith('http'):
+                    api_args['pagination'][a_key] = user_config['domain'] + api_args['pagination'][a_key]
+
+        api_args['params'] = vendor_config.get('params')
+        api_args['transform_response_data'] = vendor_config.get('transform_response_data')
+        api_args['db_model'] = self.data_type_map.get(data_type)
+        api_args['db_session'] = self.db.session()
+
+        return api_args
+
+    def simple_data_load(self, kwargs):
+        api_call_config = self.get_api_args(kwargs['data_source'], kwargs['data_type'])
+        if api_call_config is not None:
+            ad1 = ApiDataToSql(**api_call_config)
+            ad1.load_data()
 
     def load_lead_perfection(self):
         config = self.config
@@ -89,37 +282,6 @@ class DataLoadService(DbService):
 
         # load lead perfection data to db
         csv.load_data()
-
-    def load_purchases(self):
-        config = self.config
-        data_source = config.get('PURCHASE_DATA_SOURCE')
-        apicreds = config.get('EXT_DATA_CREDS')[data_source]
-        auth = (apicreds['id'], apicreds['secret'])
-        endpoint = apicreds['endpoint']
-        headers = {'Content-Type': 'application/json'}
-        params = None  # dict(ids='3211139395,3372597187')
-        db_field_map = dict(purchase_id='id',
-                            customer_id='customer.id',
-                            created_at='created_at',
-                            price='total_price',
-                            is_paid='financial_status',
-                            referring_site='referring_site',
-                            landing_site='landing_site',
-                            browser_ip='browser_ip',
-                            user_agent='client_details.user_agent')
-        primary_keys = ['purchase_id']
-        json_data_keys = 'orders'
-
-        ad1 = ApiDataToSql(endpoint=endpoint,
-                           auth=auth,
-                           headers=headers,
-                           params=params,
-                           db_session=self.db.session(),
-                           db_model=Purchase,
-                           primary_keys=primary_keys,
-                           db_field_map=db_field_map,
-                           json_data_keys=json_data_keys)
-        ad1.load_data()
 
     def load_mc_email_data(self):
         config = self.config
@@ -507,27 +669,27 @@ class DataLoadService(DbService):
 
         def load_web_tracking_data(model, dims, metrics, db_field_map):
             response = analytics.reports().batchGet(
-              body={
-                'reportRequests': [
-                {
-                  'viewId': VIEW_ID,
-                  'dateRanges': [{'startDate': startDate, 'endDate': endDate}],
-                  'metrics': [
-                      {'expression': metrics[0]},
-                      {'expression': metrics[1]},
-                      {'expression': metrics[2]}
-                  ],
-                  'dimensions': [
-                      {'name': 'ga:dimension1'},
-                      {'name': 'ga:dimension3'},
-                      {'name': 'ga:dimension2'},
-                      {'name': dims[0]},
-                      {'name': dims[1]},
-                      {'name': dims[2]},
-                      {'name': dims[3]}
-                  ]
-                }]
-              }
+                body={
+                    'reportRequests': [
+                        {
+                            'viewId': VIEW_ID,
+                            'dateRanges': [{'startDate': startDate, 'endDate': endDate}],
+                            'metrics': [
+                                {'expression': metrics[0]},
+                                {'expression': metrics[1]},
+                                {'expression': metrics[2]}
+                            ],
+                            'dimensions': [
+                                {'name': 'ga:dimension1'},
+                                {'name': 'ga:dimension3'},
+                                {'name': 'ga:dimension2'},
+                                {'name': dims[0]},
+                                {'name': dims[1]},
+                                {'name': dims[2]},
+                                {'name': dims[3]}
+                            ]
+                        }]
+                }
             ).execute()
 
             ad = ApiDataToSql(db_session=self.db.session,
@@ -543,15 +705,15 @@ class DataLoadService(DbService):
                                    dims=('ga:pagePath', 'ga:browser', 'ga:browserSize', 'ga:operatingSystem'),
                                    metrics=('ga:sessions', 'ga:pageValue', 'ga:pageviews'),
                                    db_field_map=dict(browser_id='dimensions[0]',
-                                                    utc_millisecs='dimensions[2]',
-                                                    hashed_email='dimensions[1]',
-                                                    page_path='dimensions[3]',
-                                                    browser='dimensions[4]',
-                                                    browser_size='dimensions[5]',
-                                                    operating_system='dimensions[6]',
-                                                    sessions='metrics[0].values[0]',
-                                                    page_value='metrics[0].values[1]',
-                                                    page_views='metrics[0].values[2]'))
+                                                     utc_millisecs='dimensions[2]',
+                                                     hashed_email='dimensions[1]',
+                                                     page_path='dimensions[3]',
+                                                     browser='dimensions[4]',
+                                                     browser_size='dimensions[5]',
+                                                     operating_system='dimensions[6]',
+                                                     sessions='metrics[0].values[0]',
+                                                     page_value='metrics[0].values[1]',
+                                                     page_views='metrics[0].values[2]'))
             print('successfully loaded part of web tracking data')
         except Exception as exc:
             print('failed one of the web tracking lookups: ' + str(exc))
@@ -788,3 +950,4 @@ class DataLoadService(DbService):
                         rec.__setattr__(fips_field, str(row['State FIPS Code'] + row['County FIPS Code']))
                         db.session.add(rec)
                     db.session.commit()
+
